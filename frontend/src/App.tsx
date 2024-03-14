26f103cb806076841d25ffc9d46bd049fc5474d4
Noaman Saleem <22920@umt.edu.pk>
2024-03-14 15:51:10 +0500

    Header started

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,10 +2,12 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import News from "./pages/News";
 import Contact from "./pages/Contact";
+import Header from "./components/Header";
 
 function App() {
   return (
     <Router>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/news" element={<News />} />
---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,71 @@
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import Offcanvas from "react-bootstrap/Offcanvas";
+
+function Header() {
+  return (
+    <>
+      <Navbar expand="xl" className="bg-body-tertiary mb-3">
+        <Container>
+          <Navbar.Brand href="#">Company LoGo</Navbar.Brand>
+          <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-xl`} />
+          <Navbar.Offcanvas
+            id={`offcanvasNavbar-expand-xl`}
+            aria-labelledby={`offcanvasNavbarLabel-expand-xl`}
+            placement="end"
+          >
+            <Offcanvas.Header closeButton>
+              <Offcanvas.Title id={`offcanvasNavbarLabel-expand-xl`}>
+                Offcanvas
+              </Offcanvas.Title>
+            </Offcanvas.Header>
+            <Offcanvas.Body className="justify-content-between">
+              <Nav className="justify-content-between ">
+                <Nav.Link href="#action1">Home</Nav.Link>
+
+                <NavDropdown
+                  title="Destination"
+                  id={`offcanvasNavbarDropdown-expand-xl`}
+                >
+                  <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+                  <NavDropdown.Item href="#action4">
+                    Another action
+                  </NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#action5">
+                    Something else here
+                  </NavDropdown.Item>
+                </NavDropdown>
+
+                <NavDropdown
+                  title="Category"
+                  id={`offcanvasNavbarDropdown-expand-xl`}
+                >
+                  <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+                  <NavDropdown.Item href="#action4">
+                    Another action
+                  </NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#action5">
+                    Something else here
+                  </NavDropdown.Item>
+                </NavDropdown>
+              </Nav>
+              <Nav>
+                <Nav.Link href="#action1">Login</Nav.Link>
+                <Nav.Link href="#action1">Sign up</Nav.Link>
+                <Nav.Link href="#action1">Become a partner</Nav.Link>
+              </Nav>
+            </Offcanvas.Body>
+          </Navbar.Offcanvas>
+        </Container>
+      </Navbar>
+    </>
+  );
+}
+
+export default Header;
